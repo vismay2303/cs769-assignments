849905d5009c101b81fea4802c3def8facc8c862
vismay2303 <39467568+vismay2303@users.noreply.github.com>
2023-03-01 19:29:53 -0600

    Optimizer.py ready

--- assignment2/optimizer.py
+++ assignment2/optimizer.py
@@ -1,6 +1,7 @@
 from typing import Callable, Iterable, Tuple
 
 import torch
+import math
 from torch.optim import Optimizer
 
 
@@ -38,23 +39,46 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                #raise NotImplementedError()
 
                 # State should be stored in this dictionary
                 state = self.state[p]
 
+                if len(state.keys()) == 0:
+                    state["step"] = 0
+                    state["m"] = torch.zeros_like(grad)
+                    state["v"] = torch.zeros_like(grad)
+                
+                m, v = state["m"], state["v"]
+                
+                state["step"] += 1
+
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                lr = group["lr"]
+                b1,b2 = group["betas"]
+                c_b = group["correct_bias"]
+                eps = group["eps"]
+                w_d = group["weight_decay"]
 
                 # Update first and second moments of the gradients
+                m = torch.add(torch.mul(m, b1), grad, alpha = 1 - b1)
+                v = torch.addcmul(torch.mul(v, b2), grad, grad, value = 1 - b2)
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
+                if c_b:
+                    t = state["step"]
+                    alpha = alpha * math.sqrt(1 - pow(b2,t)) / (1-pow(b1,t))
 
                 # Update parameters
-
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+                p.data = torch.addcdiv(p.data, m, torch.add(v.sqrt(),eps), value = -alpha)
+                p.data = torch.add(p.data, p.data, alpha = - w_d * lr)
+
+                state["m"] = m
+                state["v"] = v
 
         return loss
